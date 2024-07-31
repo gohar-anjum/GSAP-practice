3d43b4f5a60c90bd0718e4b2fd7e631d7b5e29ee
gohar-anjum <goharanjum49@gmail.com>
2024-07-31 23:20:27 +0500

    Gsap functions fromTo

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-gsap.to("#container", {
-    x : 500,
+gsap.fromTo ("#container", {
+    x : 100,
     duration : 2,
-    ease: Expo.easeInOut,
-    backgroudColor: "yellow"
+    ease: Expo.easeInOut
+},{
+    y : 500,
+    duration : 2,
+    backgroundColor: "black"
 })
