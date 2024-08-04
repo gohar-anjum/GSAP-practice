85c9f1ca070880bd562a2176e004070afaab62bc
gohar-anjum <goharanjum49@gmail.com>
2024-08-04 15:07:52 +0500

    Some Animations

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-gsap.from("#container", {
-    x : 700,
-    duration : 2,
-    ease: Expo.easeInOut
+gsap.to(".container:nth-child(1)", {
+  x:500,
+  duration:2,
+  delay:1,
+  repeat:-1,
+  yoyo:true
 })
+gsap.from(".container:nth-child(2)", {
+    x:500,
+    duration:2,
+    delay:1,
+    repeat:-1,
+    yoyo:true
+  })
