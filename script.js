d7a3c968baca7169be6510b1022fea2678bbeba3
gohar-anjum <goharanjum49@gmail.com>
2024-08-04 16:20:34 +0500

    Scroll Animations

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
-gsap.from(".page1 .container",{
-    opacity:0,
-    delay:1,
-    scale:1.5,
-    duration:2
-})
-gsap.from(".page2 .container",{
-    opacity:0,
-    delay:1,
-    rotate:750,
-    scale:1.5,
-    duration:2,
+gsap.to("#page2 h1",{
+    transform:"translateX(-100%)",
     scrollTrigger:{
-        trigger:".page2 .container",
+        trigger:"#page2",
         scroller:"body",
-        marker:true,
-        start:"top 60%",
-        scrub:2
+        markers:true,
+        start:"top 0%",
+        end:"top -150%",
+        scrub:2,
+        pin:true
     }
 })
